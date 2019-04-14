9b36911b378210bef4c0cd6f0614a26ae3901488
pegorino <eshkryabin@yandex.ru>
2019-04-14 17:44:24 +0300

    сделал маппер для членов семьи (пока мало функционала)

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,1 +1,1 @@
-
+from .mappers import *
--- server/models/mappers.py
+++ server/models/mappers.py
@@ -1,4 +1,5 @@
 import sqlite3
+from family import FamilyMember
 
 
 class DB:
@@ -28,7 +29,7 @@ class DB:
         try:
             params = ', '.join([key + ' ' + val for key, val in kwargs.items()])
             sql = f'CREATE TABLE IF NOT EXISTS {tb_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, {params})'
-            print(sql)
+            # print(sql)
             self.cursor.execute(sql)
             return True
         except Exception as err:
@@ -45,7 +46,7 @@ class DB:
         except sqlite3.OperationalError:
             print(f'no table <{tb_name}> detected')
 
-    def insert_into_table(self, tb_name, **kwargs):
+    def _insert_into_table(self, tb_name, **kwargs):
         '''
         добавляет запись в таблицу tb_name
         :param tb_name: имя таблицы
@@ -65,29 +66,69 @@ class DB:
         except Exception as err:
             print(err)
 
+    def _get_by_id(self, table_name, id_):
+        try:
+            self.cursor.execute(f'SELECT * FROM {table_name} WHERE id=?', (id_,))
+            result = self.cursor.fetchone()
+            return result
+        except Exception as err:
+            print(f'fetching from {tb_name} by id failed -> {err}')
+
+    def _delete_by_key(self, tb_name, param, key='id'):
+        sql = f'DELETE FROM {tb_name} WHERE {key}=?'
+        try:
+            self.cursor.execute(sql, (param,))
+            self.conn.commit()
+            return True
+        except Exception as err:
+            print(f'deleting from {tb_name} by {key} failed -> {err}')
+
+
+class FamilyMemberMapper(DB):
+
+    def __init__(self, db_name, engine='sqlite3'):
+        super().__init__(db_name, engine=engine)
+        super().connect()
+        self.tb_name = 'members'
+        self._class = FamilyMember
+
+    def insert(self, family_member: FamilyMember):
+        return self._insert_into_table(self.tb_name, **family_member.__dict__)
+
+    def get_by_id(self, id_):
+        member = self._get_by_id(self.tb_name, id_)
+        member = self._class(*member[1:])  # убираем из выборки id
+        return member
+
+    def delete_by_id(self, id_):
+        return self._delete_by_key(self.tb_name, id_, key='id')
+
 
 if __name__ == '__main__':
-    db = DB('household')
-    db.connect()
-    db.drop_table('members')
-    db.create_table(
-        'members',
-        name='VARCHAR(32)',
-        patronymic='VARCHAR(32)',
-        surname='VARCHAR(32)',
-        birthdate='VARCHAR(32)'
-    )
+    # db = DB('household')
+    # db.connect()
+    # db.drop_table('members')
+    # db.create_table(
+    #     'members',
+    #     name='VARCHAR(32)',
+    #     patronymic='VARCHAR(32)',
+    #     surname='VARCHAR(32)',
+    #     birthdate='VARCHAR(32)'
+    # )
+    #
+    # db.create_table(
+    #     'sources',
+    #     name='VARCHAR(32)',
+    #     is_regular='BOOLEAN'
+    # )
 
-    db.create_table(
-        'sources',
-        name='VARCHAR(32)',
-        is_regular='BOOLEAN'
-    )
+    mapper = FamilyMemberMapper('household')
 
-    db.insert_into_table(
-        'members_1',
-        name='Fred',
-        patronymic='D',
-        surname='Flinstone',
-        birthdate='1900-12-12'
-    )
+    father = FamilyMember('John', 'D', 'Black', '1962-12-30')
+    mapper.insert(father)
+
+    mother = FamilyMember('Mary', 'D', 'Black', '1970-1-15')
+    mapper.insert(mother)
+
+    print(mapper.get_by_id(1))
+    print(mapper.delete_by_id(1))
